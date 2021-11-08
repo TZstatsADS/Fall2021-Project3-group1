7bfd1eaace10e2b5d3903ac0f4e35ad200414810
Nikhil Cherukupalli <nikhilrcherukupalli@gmail.com>
2021-11-08 18:42:56 -0500

    added gradient oriented histograms

--- lib/build_features.py
+++ lib/build_features.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.feature import hog
 import cv2
 
 
@@ -18,15 +19,39 @@ def get_rgb_histogram(img, no_bins=5):
     return np.concatenate((feature_r, feature_g, feature_b), axis=None)
 
 
+def get_oriented_gradients_histogram(img, no_orientations=9):
+    """Wrapper around skimage.feature.hog function.
+
+    :param (array-like) img: vectorized image
+    :param (int) no_orientations: number of orientation bins
+
+    :return (array-like): feature vector using (3, 3) blocks,
+    L2 norm and normalizing image before processing.
+    """
+    feature_hog = hog(img,
+                      orientations=no_orientations,
+                      cells_per_block=(3, 3),
+                      feature_vector=True,
+                      transform_sqrt=True,
+                      block_norm="L2",
+                      multichannel=True)
+    return feature_hog
+
+
 def main():
     """TESTING CELL"""
     img_path = "../data/images/00001.png"
     img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
 
-    rgb_hist = get_rgb_histogram(img)
-    print(rgb_hist)
-    print(rgb_hist.shape)
+    # Testing RGB histogram
+    # rgb_hist = get_rgb_histogram(img)
+    # print(rgb_hist)
+    # print(rgb_hist.shape)
 
+    # Testing HOG
+    hog = get_oriented_gradients_histogram(img, 9)
+    print(hog)
+    print(hog.shape)
 
 
 if __name__ == "__main__":
