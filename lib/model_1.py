7a24c6bf78f06eb511e343c3f51bd55ab9c11215
Nikhil Cherukupalli <nikhilrcherukupalli@gmail.com>
2021-11-09 15:49:54 -0500

    modified CNN architecture, added parameter tuning routine

--- lib/model_1.py
+++ lib/model_1.py
@@ -1,7 +1,10 @@
 import os
-
+import time
 import numpy as np
 import tensorflow as tf
+import csv
+from tensorflow.keras.layers import Dense, BatchNormalization, Conv2D, MaxPooling2D, Dropout, Flatten
+from tensorflow.keras.regularizers import l2
 
 from build_features import build_features, get_rgb_histogram, get_oriented_gradients_histogram, get_texture
 from make_dataset import read_data
@@ -19,47 +22,97 @@ def sequential_ann():
     return model
 
 
-def cnn():
+def cnn(weight_decay):
     model = tf.keras.models.Sequential([
+        # Adding artificial noise by flipping, rotating, changing contrasts
         tf.keras.layers.RandomFlip("horizontal"),
         tf.keras.layers.RandomRotation(0.1),
         tf.keras.layers.RandomZoom(0.1),
         tf.keras.layers.RandomContrast(0.1),
-        tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu",
-                               input_shape=(32, 32, 3)),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Dropout(0.1),
-        tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu"),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu"),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(10, activation="softmax")
+
+        Conv2D(32, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay),
+               input_shape=(32, 32, 3)),
+        BatchNormalization(),
+        Conv2D(32, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay)),
+        BatchNormalization(),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.2),
+
+        Conv2D(64, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay)),
+        BatchNormalization(),
+        Conv2D(64, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay)),
+        BatchNormalization(),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.3),
+
+        Conv2D(128, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay)),
+        BatchNormalization(),
+        Conv2D(128, (3, 3), padding="same", activation="relu", kernel_regularizer=l2(weight_decay)),
+        BatchNormalization(),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.4),
+
+        Flatten(),
+        Dense(10, activation="softmax")
     ])
     return model
 
 
-def run_cnn():
+def run_cnn_clean(n_validation, epochs, batch_size, weight_decay, learning_rate):
+    """Running CNN model only on clean data. Useful to test if model architecture is good."""
     imgs, clean_labels, noisy_labels = read_data()
     imgs = imgs / 255.0
 
     # Split into train/test
-    n_validation = 2000
-    train_x, train_y = imgs[n_validation:], noisy_labels[n_validation:]
+    train_x, train_y = imgs[n_validation:10000], clean_labels[n_validation:]
     test_x, test_y = imgs[:n_validation], clean_labels[:n_validation]
 
     print("Training Shapes: {}, {}".format(train_x.shape, train_y.shape))
     print("Testing Shapes: {}, {}".format(test_x.shape, test_y.shape))
     # Build model
-    model = cnn()
-    # train_x, test_x = preprocessor.fit(train_x), preprocessor.fit(train_y)
-    model.compile(optimizer=tf.keras.optimizers.Adam(),
+    model = cnn(weight_decay)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                   loss="sparse_categorical_crossentropy",
-                  metrics=["accuracy"])
-    model.fit(train_x, train_y, epochs=25, batch_size=256, shuffle=True)
-    print(model.evaluate(test_x, test_y))
+                  metrics=["sparse_categorical_accuracy"])
+
+    start_time = time.time()
+    hist = model.fit(x=train_x,
+                     y=train_y,
+                     epochs=epochs,
+                     batch_size=batch_size,
+                     validation_data=(test_x, test_y))
+    end_time = time.time()
+    return hist, end_time - start_time
+
+
+def tune_cnn_params_m1_clean():
+    epochs = [10, 20, 25]
+    validation_szs = [1000, 1500]
+    learning_rates = [1e-3, 1e-4]
+    batch_size = 64
+    weight_decay = 1e-4
+
+    res_log = [["Model",
+                "# Epochs",
+                "Validation Size",
+                "Learning Rate",
+                "Weight Decay",
+                "Batch Size",
+                "Validation Loss", "Validation Accuracy", "Training Time"]]
+    for epoch in epochs:
+        for n_validation in validation_szs:
+            for lr in learning_rates:
+                hist, time = run_cnn_clean(n_validation, epoch, batch_size, weight_decay, lr)
+                res = ["CNN", epoch, n_validation, lr, weight_decay, batch_size,
+                       hist["val_loss"][-1], hist["val_sparse_categorical_accuracy"][-1], time]
+                res_log.append(res)
+                print(res)
+
+    # Write output to disk
+    file = open("../output/cnn_clean_tests.csv", "w+", newline="")
+    with file:
+        write = csv.writer(file)
+        write.writerows(res_log)
 
 
 def run_sequential_ann():
@@ -92,4 +145,4 @@ def run_sequential_ann():
 
 
 if __name__ == "__main__":
-    run_cnn()
+    tune_cnn_params_m1_clean()
