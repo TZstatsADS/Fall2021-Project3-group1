f1703cef75b49344e420818f2351ba7bd3850e99
Nikhil Cherukupalli <nikhilrcherukupalli@gmail.com>
2021-11-08 19:17:07 -0500

    added Harlick's, wrapper to build many features over matrix

--- lib/build_features.py
+++ lib/build_features.py
@@ -1,13 +1,16 @@
 import numpy as np
 from skimage.feature import hog
 import cv2
+import mahotas
+from mahotas.features import haralick
+import timeit
 
 
 def get_rgb_histogram(img, no_bins=5):
-    """ Returns array of RGB histogram.
+    """Returns array of RGB histogram.
 
-    :param img (array-like): vectorized image
-    :param no_bins (int): number of bins for hist for each color
+    :param (array-like) img : vectorized image
+    :param (int) no_bins : number of bins for hist for each color
 
     :return (array-like): array of shape (no_bins * 3, )
     """
@@ -38,20 +41,57 @@ def get_oriented_gradients_histogram(img, no_orientations=9):
     return feature_hog
 
 
+def get_texture(img):
+    """Returns array of texture feature.
+
+    :param (array-like) img: vectorized image
+
+    :return (array-like): feature vector of shape (13, )
+    """
+    # Setting image to grayscale, then filtering using Gaussian filter
+    img_filtered = img[:, :, 0]
+    img_filtered = mahotas.gaussian_filter(img_filtered, 4)
+    thresh = (img_filtered > img_filtered.mean())
+    labeled, n = mahotas.label(thresh)
+
+    feature_texture = mahotas.features.haralick(labeled).mean(axis=0)
+    return feature_texture
+
+
+def build_features(imgs_mtx, feature_fns):
+    feature_mtx = list()
+    for img in imgs_mtx:
+        features = [f(img) for f in feature_fns]
+        features_concat = np.concatenate(features, axis=None)
+        feature_mtx.append(features_concat)
+    return np.array(feature_mtx)
+
+
 def main():
-    """TESTING CELL"""
-    img_path = "../data/images/00001.png"
-    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+    # Collect data
+    print("Collecting data")
+    collection_start = timeit.timeit()
+    n_img = 50000
+    imgs = np.empty((n_img, 32, 32, 3))
+    for i in range(n_img):
+        img_fn = f'../data/images/{i + 1:05d}.png'
+        imgs[i, :, :, :] = cv2.cvtColor(cv2.imread(img_fn), cv2.COLOR_BGR2RGB)
+    collection_end = timeit.timeit()
 
-    # Testing RGB histogram
-    # rgb_hist = get_rgb_histogram(img)
-    # print(rgb_hist)
-    # print(rgb_hist.shape)
+    # Build features
+    print("Building features")
+    features_start = timeit.timeit()
+    feature_mtx = build_features(imgs, [get_rgb_histogram,
+                                        get_oriented_gradients_histogram,
+                                        get_texture])
+    features_end = timeit.timeit()
 
-    # Testing HOG
-    hog = get_oriented_gradients_histogram(img, 9)
-    print(hog)
-    print(hog.shape)
+    # Print summary stats
+    print("Original matrix shape: {}".format(imgs.shape))
+    print("Processed matrix shape: {}".format(feature_mtx.shape))
+
+    print("Time Elapsed (Data collection): {}".format(collection_end - collection_start))
+    print("Time Elapsed (Feature Engineering): {}".format(features_end - features_start))
 
 
 if __name__ == "__main__":
